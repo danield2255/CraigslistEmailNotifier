0e146851e3fd1080b051b95c16be0233394dfe07
danield2255 <danield2255@gmail.com>
2019-11-30 10:09:02 -0800

    commit of general functionality

--- ScraperNotifier.py
+++ ScraperNotifier.py
@@ -12,25 +12,31 @@ from selenium.webdriver.common.keys import Keys
 import bs4 as bs
 from bs4 import BeautifulSoup, element
 
-emailAddress = os.environ.get('music_email_user')
-emailPass = os.environ.get('music_email_pass')
+#input own email and password
+emailAddress = #enter email
+emailPass = #enter pass
 
 links = []
+#Can choose to use chrome if phantomjs is not functional
 option = webdriver.ChromeOptions()
 option.add_argument("--incognito")
 option.add_argument('--no-sandbox')
+option.add_argument('headless')
 #option.add_argument("--no-startup-window")
-wd = webdriver.Chrome("../../Documents/chromedriver", options = option)
-wd.get("https://slo.craigslist.org/")
+#wd = webdriver.Chrome("dependencies/chromedriver", options = option)
+wd = webdriver.PhantomJS("dependencies/phantomjs")
+#Enter the particular craigslist site desired in quotes
+wd.get("""url""")
 gigsButton = wd.find_element_by_xpath('//*[@id="ggg"]/h4/a')
 gigsButton.click()
 time.sleep(2)
-print(wd.current_url)
+
 searchBar = wd.find_element_by_xpath('//*[@id="query"]')
-searchBar.send_keys("music")
+#enter the value of what you would like to search for
+searchBar.send_keys("""search""")
 searchBar.send_keys(Keys.ENTER)
 time.sleep(2)
-print(wd.current_url)
+
 soup = bs.BeautifulSoup(wd.page_source, "html.parser")
 listings = soup.find_all("a", attrs = {'class':"result-title hdrlnk"})
 for listing in listings:
@@ -46,17 +52,18 @@ msg = EmailMessage()
 msg["Subject"] = "Update of Craigslist Listing"
 msg["From"] = emailAddress
 msg["To"] = emailAddress
-content = "This is your update of live music wanted in slo:"
+#put in the content of text in the email
+content = #content
 total = ""
 for item in links:
     #cur = item.format(link = item.get("href"), text = item.text)
     total = total + str(item) + "<br><br>"
-print(total)
+
 
 msg.set_content(MIMEText(content + total, "html"))
 
 
-
+#this will work if the email you are trying to send from is a gmail, will need to change otherwise
 with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
     smtp.login(emailAddress, emailPass)
     smtp.send_message(msg)
